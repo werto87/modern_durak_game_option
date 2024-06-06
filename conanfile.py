37b334f750c5cd3d023e978591a1db5c882af486
werto87 <huhulasmichinruhe@web.de>
2024-06-06 17:34:48 +0200

    check if game lobby is valid

--- conanfile.py
+++ conanfile.py
@@ -3,12 +3,17 @@ from conan import ConanFile
 
 class Project(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    generators = "CMakeToolchain", "CMakeDeps"
+    generators =  "CMakeDeps"
 
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.user_presets_path = False #workaround because this leads to useless options in cmake-tools configure
+        tc.generate()
+        
     def configure(self):
         self.options["catch2"].with_main = True
         self.options["catch2"].with_benchmark = True
 
     def requirements(self):
-        self.requires("durak/1.0.3")
-        self.requires("boost/1.84.0")
+        self.requires("durak/1.0.5")
+        self.requires("boost/1.85.0")
--- modern_game_game_option/userDefinedGameOption.hxx
+++ modern_game_game_option/userDefinedGameOption.hxx
@@ -1,35 +1,45 @@
 #pragma once
 
- #include <boost/fusion/adapted/struct/define_struct.hpp>
-  #include <durak/gameOption.hxx>
- #include <optional>
- #include <string>
+#include <boost/fusion/adapted/struct/define_struct.hpp>
+#include <durak/gameOption.hxx>
+#include <optional>
+#include <string>
 
- typedef std::vector<std::pair<std::string, long long int>> UserTimeMilliseconds;
+typedef std::vector<std::pair<std::string, long long int> > UserTimeMilliseconds;
 
- namespace shared_class
+namespace shared_class
 {
- enum struct TimerType
+enum struct TimerType
 {
-   noTimer,
-   resetTimeOnNewRound,
-   addTimeOnNewRound
- };
- }
+  noTimer,
+  resetTimeOnNewRound,
+  addTimeOnNewRound
+};
+}
 
-  BOOST_FUSION_DEFINE_STRUCT ((shared_class), TimerOption, (shared_class::TimerType, timerType) (uint64_t, timeAtStartInSeconds) (uint64_t, timeForEachRoundInSeconds))
- namespace shared_class
+BOOST_FUSION_DEFINE_STRUCT ((shared_class), TimerOption, (shared_class::TimerType, timerType) (uint64_t, timeAtStartInSeconds) (uint64_t, timeForEachRoundInSeconds))
+namespace shared_class
 {
- enum struct OpponentCards
+enum struct OpponentCards
 {
-   showNumberOfOpponentCards,
-   showOpponentCards,
- };
- }
- BOOST_FUSION_DEFINE_STRUCT ((shared_class), GameOption, (durak::GameOption, gameOption) (shared_class::TimerOption, timerOption) (uint64_t, computerControlledPlayerCount) (shared_class::OpponentCards, opponentCards)) // TODO-TEMPLATE add game options
+  showNumberOfOpponentCards,
+  showOpponentCards,
+};
 
- std::optional<std::string> inline errorInGameOption (shared_class::GameOption const &)
+}
+BOOST_FUSION_DEFINE_STRUCT ((shared_class), GameOption, (durak::GameOption, gameOption) (shared_class::TimerOption, timerOption) (uint64_t, computerControlledPlayerCount) (shared_class::OpponentCards, opponentCards))
+
+namespace shared_class
 {
-   // TODO-TEMPLATE check Game option
-   return std::nullopt;
- }
+std::optional<std::string> inline errorInGameOption (shared_class::GameOption const &gameOption)
+{
+  if (gameOption.computerControlledPlayerCount >= 2)
+    {
+      return "only one computer controlled player per game";
+    }
+  else
+    {
+      return std::nullopt;
+    }
+}
+}
